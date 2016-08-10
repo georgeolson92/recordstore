37c6c55cee9314c20d4f600e2650fcb8abd17e4d
George Olson and Terri Bradley <george+terri@>
2016-08-10 16:22:19 -0700

    update app component

--- app/app.component.ts
+++ app/app.component.ts
@@ -16,8 +16,10 @@ export class AppComponent {
   constructor(){
     this.albums = [
       new Album("Contra", "Vampire Weekend", "Alternative", 19.99),
-      new Album("Emotion", "Carly Rae Jepsen", "Pop", 18.99),
-      new Album("Good Kid, M.A.A.D City", "Kendrick Lamar", "Rap", 19.99)
+      new Album("Dangerous Woman", "Ariana Grande", "Pop", 18.99),
+      new Album("Good Kid, M.A.A.D City", "Kendrick Lamar", "Rap", 19.99),
+      new Album("Panda Bear Meets The Grim Reaper", "Panda Bear", "Electronic", 19.99),
+      new Album("Lemonade", "Beyonce", "R&B", 35.99)
     ];
   }
   albumWasSelected(clickedAlbum: Album): void {
